0e1293e12885be95599f5e24ac03a4563b5ab5da
Ricardo Cerqueira <ricardo@cyngn.com>
2016-04-10 00:35:46 +0100

    taido_row: power: Add a basic power HAL

--- full_taido_row.mk
+++ full_taido_row.mk
@@ -77,6 +77,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf	
 
+# Power
+PRODUCT_PACKAGES += \
+    power.default \
+    power.mt6735m
 
 # Telecomm
 PRODUCT_COPY_FILES += \
