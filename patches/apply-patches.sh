eaa5bd59826a59203a02485a4d05042f4c93595b
SamarV-121 <samarvispute121@gmail.com>
2017-09-07 12:29:25 +0530

    taido_row: patches: update

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -4,7 +4,7 @@ cd system/core
 patch -p1 < ../../device/motorola/taido_row/patches/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 patch -p1 < ../../device/motorola/taido_row/patches/0004-libnetutils-add-MTK-bits-ifc_ccmni_md_cfg.patch
 patch -p1 < ../../device/motorola/taido_row/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
-#patch -p1 < ../../device/motorola/taido_row/patches/0015-healthd_batteryVoltage.patch
+patch -p1 < ../../device/motorola/taido_row/patches/0015-healthd_batteryVoltage.patch
 cd ../..
 cd system/netd
 patch -p1 < ../../device/motorola/taido_row/patches/0010-wifi-tethering-fix.patch
@@ -21,7 +21,8 @@ patch -p1 < ../../device/motorola/taido_row/patches/0007-Disable-usage-of-get_ca
 patch -p1 < ../../device/motorola/taido_row/patches/0008-Partial-Revert-Camera1-API-Support-SW-encoders-for-n.patch
 patch -p1 < ../../device/motorola/taido_row/patches/0009-add-mtk-color-format-support.patch
 patch -p1 < ../../device/motorola/taido_row/patches/0013-fix_video_autoscaling.patch
+patch -p1 < ../../device/motorola/taido_row/patches/0016-frameworks_av_media.patch
 cd ../..
 cd frameworks/base
-patch -p1 < ../../device/motorola/taido_row/patches/014-add_procged_to_the_FD_whitelist.patch
+patch -p1 < ../../device/motorola/taido_row/patches/0014-add_procged_to_the_FD_whitelist.patch
 cd ../..
